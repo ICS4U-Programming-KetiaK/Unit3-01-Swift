ebb9a16abbae9dca48a33a4f6b5f58bbcfebad87
ketiagaelle <>
2022-05-13 13:40:14 +0000

    Created empty constructors.

---
+++ MyIntStack.swift
@@ -0,0 +1,17 @@
+//
+// MyIntStack.swift
+//
+// Created by Ketia Gaelle Kaze
+// Created on 2022-04-25
+// version 1.0
+// Copyright (c) 2022 IMH. All rights reserved.
+//
+// MyIntStack creates an empty constructor.
+import Foundation
+
+class MyIntStack {
+  // empty constructor
+  init() { }
+}
+// creating a new object for the integer stack
+var myIntStack = MyIntStack()
---
+++ MyStringStack.swift
@@ -0,0 +1,17 @@
+//
+// MyStringStack.swift
+//
+// Created by Ketia Gaelle Kaze
+// Created on 2022-04-25
+// version 1.0
+// Copyright (c) 2022 IMH. All rights reserved.
+//
+// MyStringStack creates an empty constructor.
+import Foundation
+
+class MyStringStack {
+  // empty constructor
+  init() { }
+}
+// creating a new object for the string stack
+var myStringStack = MyStringStack()
